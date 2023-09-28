b8b0d65d023d2e44efbb36a6d32f6281924b305d
Victor Barros <victoredbc@hotmail.com>
2023-09-28 19:43:47 -0300

    Add execute validations

--- source/momon.py
+++ source/momon.py
@@ -13,7 +13,7 @@ class ClassLove:
     
     def execute(self, momon: Momon) -> str:
         love = random.randint(0, 100)
-        if love > 2:
+        if love > 2 and momon.happyness:
             return f'I love Momon {love}% more than the Class'
         
         return f'I love this class {love}% more then the famous legendary Momon'
